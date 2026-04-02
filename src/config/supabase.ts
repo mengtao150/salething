40c1461a3554d40bff1146930bf1d40dd80b8579
WilliamTao <2640622464@qq.com>
2026-04-02 09:38:48 +0800

    Update Supabase anon key

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -2,5 +2,5 @@
 // 请填入你的 Supabase 项目信息
 export const supabaseConfig = {
   url: 'https://suvsvigufwvfvyllscka.supabase.co',
-  anonKey: 'sb_publishable_Xf8AxbaBiXXVK_EEWFrH3A_1Bth4dpl'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1dnN2aWd1Znd2ZnZ5bGxzY2thIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0NzAwNjMsImV4cCI6MjA5MDA0NjA2M30.fx-WGPWj-GbPp60VnMz900KbM-7fR4mozYxdXZWX0Sg'
 }
